0f3e3dc80f116ead757bde89f0125c4d0619d98f
Corentin PACAUD <pacaudcorentin.contact@gmail.com>
2021-12-10 00:08:19 +0100

    add electron app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,40 +9,7 @@ export class AppComponent {
   title = 'tournoiinterne';
 
 
-  arrPlayers: String[] = ['coco', 'nico', 'laurent', 'Alex', 'Random Names',
-    'Carl Mendez',
-    'Rabia Ward',
-    'Keelan Lowe',
-    'Stefano Higgs',
-    'Rohan Prosser',
-    'Fahima Hodson',
-    'Sabrina Kim',
-    'Tre Molina',
-    'Asiyah Tyler',
-    'Arslan Mcgill',
-    'Lubna Pitts',
-    'Byron Chandler',
-    'Osian Alvarez',
-    'Dave Schultz',
-    'Aleyna Bennett',
-    'Charlize Hobbs',
-    'Natan Neal',
-    'Warwick Blackmore',
-    'Aidan Lott',
-    'Aaran Davidson',
-    'Jibril Haigh',
-    'Raife Barnard',
-    'Makayla Samuels',
-    'Ahyan Noble',
-    'Rick Padilla',
-    'Mahdi Stubbs',
-    'Donte Copeland',
-    'Diya Jordan',
-    'Aisling Hensley',
-    'Logan Herrera',
-    'Nafisa Ritter',
-    'Kenzie Randolph',
-  ]
+  arrPlayers: String[] = []
   inputPlayer: String = "";
 
   matches: any[] = []
@@ -66,11 +33,19 @@ export class AppComponent {
       let p1 = this.popRandom(players)
       let p2 = this.popRandom(players)
       let p3 = this.popRandom(players)
-      courts.push({ 'name': 'terrain ' + (i + 1), p0, p1, p2, p3 })
+      courts.push({ 'name': 'terrain ' + (i + 1), p0, p1, p2, p3, "minimized": false })
     }
     courts.push({ 'name': "Repos", players })
     console.log(courts)
     this.matches.push(courts)
+  }
+
+  deleteMatch(index: number) {
+    this.matches.splice(index, 1);
+  }
+
+  minimizeMatch(index: number) {
+    this.matches[index].minimized = !this.matches[index].minimized
   }
 
   popRandom(array: any[]) {
